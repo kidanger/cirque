640196b19bc532e8937e3b8fb043827c094d9e90
Anger Jérémy <angerj.dev@gmail.com>
2024-07-28 17:09:50 +0200

    draft

--- cirque/src/main.rs
+++ cirque/src/main.rs
@@ -1,15 +1,92 @@
-use std::fs::File;
 use std::io::BufReader;
 use std::sync::Arc;
+use std::{collections::HashMap, fs::File};
 mod config;
 use anyhow::{anyhow, Ok};
+use cirque_parser::Message;
 use std::{path::PathBuf, str::FromStr};
 use tokio::io::AsyncReadExt;
 use tokio::net::TcpListener;
 use tokio_rustls::{rustls, TlsAcceptor};
 
+type UserID = usize;
+type ChannelID = String;
+
+struct User {
+    id: UserID,
+    username: Vec<u8>,
+    ip: String,
+    mailbox: tokio::sync::mpsc::UnboundedSender<MessageToSend>,
+}
+
+struct ConnectingUser {
+    cap: Option<Vec<String>>,
+    nick: Option<String>,
+    user: Option<String>,
+}
+
+struct Channel {
+    name: String,
+    users: Vec<UserID>,
+}
+
+impl Channel {}
+
+struct ServerState {
+    users: HashMap<UserID, User>,
+    connecting_users: Vec<ConnectingUser>,
+    channels: HashMap<ChannelID, Channel>,
+}
+
+struct ConnectingSession {
+    stream: tokio_rustls::server::TlsStream<tokio::net::TcpStream>,
+}
+
+enum IRCMessage {
+    PRIVMSG(String, String),
+    PING,
+    PONG,
+}
+
+impl TryFrom<cirque_parser::Message<'_>> for IRCMessage {
+    type Error = anyhow::Error;
+
+    fn try_from(value: cirque_parser::Message) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
+
+impl ConnectingSession {
+    pub async fn deal_with_connection(&mut self) -> Result<Session, anyhow::Error> {
+        let chosen_nick: Option<String> = None;
+        let chosen_user: Option<String> = None;
+        let mut buffer = Vec::with_capacity(512);
+        loop {
+            let _size = self.stream.read_buf(&mut buffer).await?;
+
+            let (buffer, message) = cirque_parser::parse_message(&buffer)?;
+            let message = message.try_into()?;
+
+            match message {
+                IRCMessage::PRIVMSG(_, _) => todo!(),
+                IRCMessage::PING => todo!(),
+                IRCMessage::PONG => todo!(),
+                _ => anyhow::bail!("illegal command during connection"),
+            }
+
+            if message.command() == b"MESSAGE" {
+                channel = state.get_channel(command.args[0]);
+                channel.send_message_to_everyone(command.args[1..]);
+            }
+        }
+
+        Ok(Session::new())
+    }
+}
+
 struct Session {
     buffer: Vec<u8>,
+    mailbox: tokio::sync::mpsc::UnboundedReceiver<MessageToSend>,
 }
 
 impl Session {
@@ -25,12 +102,21 @@ impl Session {
     ) -> Result<(), anyhow::Error> {
         let _size = stream.read_buf(&mut self.buffer).await?;
 
+        if command == "MESSAGE" {
+            channel = state.get_channel(command.args[0]);
+            channel.send_message_to_everyone(command.args[1..]);
+        }
+
+        let user: User;
+        user.mailbox.send(message_to_send);
+
         Ok(())
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    dbg!("hello world");
     let config_path = PathBuf::from_str("ircd.yml")?;
     let c = config::Config::load_from_path(&config_path)?;
 
@@ -63,6 +149,9 @@ async fn main() -> Result<(), anyhow::Error> {
 
             let fut = async move {
                 let mut stream = acceptor.accept(stream).await?;
+
+                let session = ConnectingSession { stream };
+                let session = session.deal_with_connection();
 
                 loop {
                     session.read_buf(&mut stream).await?;
